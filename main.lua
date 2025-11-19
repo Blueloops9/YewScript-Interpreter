f3ee7703520c52d91f41f2e6d5875dbeefc56c79
Blueloops9 <DarkBlueStealth@protonmail.com>
2025-11-19 02:22:19 -0500

    Update main.lua
    Updated the code to follow the interpreters new execution logic

--- main.lua
+++ main.lua
@@ -2,10 +2,7 @@ local loadyewscript = require("yewscript")
 
 
 local Code = "printhello world!"
+local Yewscript = loadyewscript()(Code)
+local Stop=false
 
-local Time = os.clock()
-local Yewscript = loadyewscript(Code)
-print("Compile: ",os.clock()-Time)
-local Time = os.clock()
-Yewscript()
-print("Execution:",os.clock()-Time)
+repeat Stop=Yewscript() until Stop
